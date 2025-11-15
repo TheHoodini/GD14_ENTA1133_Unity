7bb9b09b66532ab63842f9fa88d67b3c3e0e227a
Juan Diego García <salasjuan@uninorte.edu.co>
2025-11-14 18:20:04 -0800

    feat: more search functions

--- Project1/Assets/Scripts/Rooms/RoomTreasure.cs
+++ Project1/Assets/Scripts/Rooms/RoomTreasure.cs
@@ -2,8 +2,21 @@ using UnityEngine;
 
 public class RoomTreasure : RoomBase
 {
+
+    [SerializeField] private int treasureID = 1;
     public override void OnRoomSearch()
     {
-        Debug.Log($"You search... And find a shiny treasure!");
+        switch (treasureID)
+        {
+            case 1:
+                Debug.Log($"You search... And you find yourself in a treasure room!");
+                break;
+            case 2:
+                Debug.Log($"You search... And find a precious treasure!");
+                break;
+            default:
+                Debug.Log($"You search... And find a pile of gold coins!");
+                break;
+        }
     }
 }
